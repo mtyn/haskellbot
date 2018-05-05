7eda923479cb708acef4eb6d755822ebe39b96b5
Tim Hull <tim@mtyn.uk>
2018-05-05 11:06:59 +0100

    🎉 Initial commit and add basic js

---
+++ js/haskellRunner.js
@@ -0,0 +1,1 @@
+let spawn = require()
---
+++ js/index.js
@@ -0,0 +1,15 @@
+import { secrets } from '../secrets/secrets.js';
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login(secrets.botToken);
